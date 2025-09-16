8dbae36c28c6d7f7261d32e092bed317ab7cca6d
Salem Alswil <sa.al9wel@gmail.com>
2025-09-16 20:34:51 +0300

    To Do List Using React

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <>
+      <div className="app">
+        <h1 className="text-3xl text-center font-bold ">مرحبا اسمي سالم</h1>
+      </div>
+    </>
+  )
+}
+export default App
